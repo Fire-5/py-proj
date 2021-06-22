d53db6899c886df493c7545efa6991a3e15247b6
Fire-5 <alexandr_krasnot@mail.ru>
2021-06-22 19:28:58 +0700

    3

--- temp_01.py
+++ temp_01.py
@@ -26,12 +26,12 @@ import time
 start = time.time() # Время начала отсчета
 
 urls = [
-    ['http://www.google.com', '172.217.18.110', 80],
-    ['http://www.yandex.ru', '77.88.55.55'   , 80],
-    ['http://www.python.org', '138.197.63.241', 80],
-    ['http://vk.com', '87.240.190.67', 80],
-    ['http://www.youtube.com', '142.250.181.238', 80],
-    ['http://mail.ru', '94.100.180.201', 80]
+    ['https://www.google.com/', '172.217.18.110', 443],
+    ['https://yandex.ru/', '77.88.55.66'   , 443],
+    ['https://www.python.org/', '138.197.63.241', 443],
+    ['https://vk.com/fire_5', '87.240.190.67', 443],
+    ['https://www.youtube.com/', '142.250.181.238', 443],
+    ['https://mail.ru/', '94.100.180.201', 443]
 ]
 
 
@@ -40,11 +40,13 @@ urls = [
 def tic():
     return 'at %1.3f seconds' % (time.time() - start)
 
-async def send(socket, addres, request):
+async def send(socket, url):
+    req = 'GET /index.php HTTP/1.1\nHost: {}\nContent-Type: text/html\nConnection: close\n\n'.format(url[0]).encode()
     # Подключаемся по заранее указанному адресу
+    addres = (url[1],url[2])
     socket.connect(addres)
     # Передаем HTTP Запрос.
-    socket.send(request)
+    socket.send(req)
     print('... Done')
 
 
@@ -64,32 +66,25 @@ async def call_url_socet(url):
     print('Starting ', url[0], end='')
     # Создаем объект сокета для подключения
     connect_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    addr = (url[1],url[2])
-    req = 'GET / HTTP/1.1\nHost: {}\nContent-Type: text/html\nConnection: close\n\n'.format(url[0]).encode()
-    await send(connect_socket, addr, req)
+
+    await send(connect_socket, url)
 
     data = await listen(connect_socket)
     print(' ---- ', tic(), url[0], 'bytes:', len(data), data[:50], '\n')
     return data
 
+#####################################################
 
 async def call_url(url):
     print('Starting ', url[0])
-    try:
-        # Асинхронный запрос 'GET' к сайту по url представить как "запрос"
-        async with aiohttp.request('GET', url[0]) as response:
-        #response = await aiohttp.get(url) # Не работает на py3.6
-            # Получаем данные в виде текста
-            st = await response.status
-            data = await response.text()
-            # Выводим результат получения данных
-            print('{}:{}: status: {} bytes: {} '.format(tic(),
-                                                        url[0],
-                                                        st,
-                                                        len(data)))
-
-    except:
-        print(' >>>> {} is unresponsive'.format(url[0]))
+    # Асинхронный запрос 'GET' к сайту по url представить как "запрос"
+    async with aiohttp.request('GET', url[0]) as response:
+    #response = await aiohttp.get(url) # Не работает на py3.6
+        # Получаем данные в виде текста
+        st = response.status
+        data = await response.text()
+        # Выводим результат получения данных
+        print('{}:{}: status:{} bytes: {} '.format(tic(), url[0],st, len(data)))
     response.close()
 
 
@@ -100,7 +95,7 @@ futures1 = [call_url(url) for url in urls]
 futures2 = [call_url_socet(url) for url in urls]
 # Запуск в цикле асихронных функций
 loop.run_until_complete(asyncio.wait(futures1))
-loop.run_until_complete(asyncio.wait(futures2))
+# loop.run_until_complete(asyncio.wait(futures2))
 # Как только закончатся выполнения функций, закрыть цикл
 loop.close()
 
